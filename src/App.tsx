608ccde2493050c215ce44b8dd65fa6c41500aff
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-01-26 20:54:57 +0100

    reorganize the codebase and implement router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TodoList from "./TodoList"
+import LandingPage from "./pages/LandingPage";
 
 function App(){
   
   return (
-    <div>
-      <TodoList />
-    </div>      
+     <div className="w-full min-h-[100vh] bg-[#181819] text-black">
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/todolist" element={<TodoList />} />
+          </Routes>
+        </Router>
+      </div>      
   )
 }
 
