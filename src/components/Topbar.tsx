554242b2e027c7e721bb17445b36b40212acb311
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-01-27 23:53:11 +0100

    topbar component for large screen

---
+++ src/components/Topbar.tsx
@@ -0,0 +1,13 @@
+
+import avather from "../assets/avather.png";
+
+
+const Topbar= () => {
+    return (
+      <div>
+        <img className="w-[40px] h-[40px]" src={avather} alt="task" />
+      </div>
+    );
+}
+
+export default Topbar
