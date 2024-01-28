d645fa7d7fa569af389d60151d4407408a6178ec
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-01-28 22:04:03 +0100

    clean the code

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,7 +1,7 @@
 import {FC, useEffect, useState} from 'react'
 import DashboardLayout from '../layout/DashboardLayout'
 import Topbar from '../components/Topbar';
-// import { BsPlusCircle } from 'react-icons/bs';
+
 
 import "../App.css"
 
@@ -61,54 +61,17 @@ const Tasks: FC = () => {
           <br /> Let’s Do This!!!
         </div>
         <div className="mt-10">
-          {/* <div>
-            <h1 className="text-[#56CFE1] text-[13px]">Todo</h1>
-            <div className="w-[100px]  md:w-[150px] h-[70px] border-[1px] border-[#56CFE1] border-dashed flex justify-center items-center cursor-pointer md:mt-2 rounded-md">
-              <BsPlusCircle className="text-[#56CFE1]" />
-            </div>
-          </div> */}
         
         </div>
-         <div className="flex gap-[20px] mt-10">
-          <div className="w-[100px]  md:w-[150px] h-fit p-2 md:mt-2 rounded-md bg-gray-600 ">
-            <div className="flex justify-between items-center">
-              <h1 className="text-black">12 Tasks</h1>
-              <div className="h-[10px] w-[10px] rounded-full bg-[#BD6AF7]"></div>
-            </div>
-            <h1 className="font-bold">Personal</h1>
-            <div className="w-full h-[5px] bg-white rounded-md">
-              <div className="bg-[#052659] h-[5px] w-[80px]"></div>
-            </div>
-          </div>
-          <div className="w-[100px]  md:w-[150px] h-fit p-2 md:mt-2 rounded-md bg-gray-600 ">
-            <div className="flex justify-between items-center">
-              <h1 className="text-black">4 Tasks</h1>
-              <div className="h-[10px] w-[10px] rounded-full bg-[#FBC02D]"></div>
-            </div>
-            <h1 className="font-bold">Work</h1>
-            <div className="w-full h-[5px] bg-white rounded-md">
-              <div className="bg-[#052659] h-[5px] w-[80px]"></div>
-            </div>
-          </div>
-          <div className="w-[100px]  md:w-[150px] h-fit p-2 md:mt-2 rounded-md bg-gray-600 ">
-            <div className="flex justify-between items-center">
-              <h1 className="text-black">17 Tasks</h1>
-              <div className="h-[10px] w-[10px] rounded-full bg-[#7CB342]"></div>
-            </div>
-            <h1 className="font-bold">Family</h1>
-            <div className="w-full h-[5px] bg-white rounded-md">
-              <div className="bg-[#052659] h-[5px] w-[80px]"></div>
-            </div>
-          </div>
-        </div>
-      <div className=" w-[380px] md:w-[550px]  ">
+  
+      <div className=" w-[350px] md:w-[550px]  ">
       <div className="">
-        <input className="w-full rounded-full  h-10 bg-transparent border-[0px] mt-10 text-white outline-none" type="text" placeholder="Add tasks..." value={newTask}
+        <input className="w-full rounded-full  h-10 bg-transparent border-[0px] mt-5 text-white outline-none" type="text" placeholder="Add tasks..." value={newTask}
         onKeyDown={handleKeyDown}
           onChange={(e) => setNewTask(e.target.value)} />
       </div>
 
-      <div className=" mt-10 text-white main overflow-scroll  ">
+      <div className=" mt-5 text-white main overflow-scroll  ">
      <ul>
         {tasks.map(task => (
               <li
