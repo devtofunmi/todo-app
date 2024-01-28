bf86abc4c6dd29b8d437d7e19c879d81ac33413e
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-01-28 01:19:31 +0100

    chage the styling

--- src/components/MobileSidebar.tsx
+++ src/components/MobileSidebar.tsx
@@ -1,61 +1,61 @@
 import {FC} from "react";
 import { Link } from "react-router-dom";
-import { MdSpaceDashboard } from "react-icons/md";
 import { BiTask } from "react-icons/bi";
 import { CgNotes } from "react-icons/cg";
-import { AiOutlineSetting } from "react-icons/ai";
-import { BiHelpCircle } from "react-icons/bi";
+import { IoTrashBinOutline } from "react-icons/io5";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { GoArchive } from "react-icons/go";
 
 const MobileSidebar: FC = () => {
   return (
-    <div className=" h-[500px] text-white p-3 bg-[#181819]  w-[280px] z-20 flex flex-col justify-center items-center  border-[1px] border-white border-opacity-10">
+    <div className=" h-[380px] text-white p-3 bg-[#181819]  w-[250px] z-20 flex flex-col justify-left   border-[1px] border-white border-opacity-10">
       <div>
         {/* <img className="w-[100px]" src={dashboadlogo} alt="dashboard_logo" /> */}
         {/* <h1>Notes</h1> */}
       </div>
-      <div className="mt-7 flex gap-3 items-center py-2 px-4 border-white border-[1px] rounded-lg cursor-pointer hover:bg-[#4cbf87] ">
-        <MdSpaceDashboard
-          size={25}
-          className="hover:text-blue-500 text-white cursor-pointer "
-        />
-        <Link to={"/"}>
-          <h1 className="text-md">Dashboard</h1>
-        </Link>
-      </div>
-      <div className="mt-5 flex gap-3 items-center py-2 px-9 border-white border-[1px] rounded-lg cursor-pointer hover:bg-[#4cbf87] ">
-        <BiTask
-          size={25}
-          className="hover:text-blue-500 text-white cursor-pointer "
-        />
-        <Link to={"/task"}>
-          <h1 className="text-md">Tasks</h1>
-        </Link>
-      </div>
-      <div className="mt-5 flex gap-3 items-center py-2 px-8 border-white border-[1px] rounded-lg cursor-pointer hover:bg-[#4cbf87] ">
+      <div className="mt-7 flex gap-3 items-center   rounded-lg cursor-pointer hover:text-[#4cbf87] ">
         <CgNotes
           size={25}
-          className="hover:text-blue-500 text-white cursor-pointer "
+          className="hover:text-[#4cbf87] text-white cursor-pointer "
         />
         <Link to={"/note"}>
           <h1 className="text-md">Notes</h1>
         </Link>
       </div>
-      <div className="flex gap-3 items-center py-2 px-8 border-white border-[1px] rounded-lg cursor-pointer hover:bg-[#4cbf87]  mt-20">
-        <AiOutlineSetting
+      <div className="mt-5 flex gap-3 items-center cursor-pointer hover:text-[#4cbf87] ">
+        <BiTask
           size={25}
-          className="hover:text-blue-500 text-white cursor-pointer "
+          className="hover:text-[#4cbf87] text-white cursor-pointer "
         />
-        <Link to={"/"}>
-          <h1 className="text-md">Settings</h1>
+        <Link to={"/task"}>
+          <h1 className="text-md">Tasks</h1>
         </Link>
       </div>
-      <div className="mt-5 flex gap-3 items-center py-2 px-4 border-white border-[1px] rounded-lg cursor-pointer hover:bg-[#4cbf87] ">
-        <BiHelpCircle
+      <div className="mt-5 flex gap-3 items-center cursor-pointer hover:text-[#4cbf87] ">
+        <IoMdNotificationsOutline 
           size={25}
-          className="hover:text-blue-500 text-white cursor-pointer "
+          className="hover:text-[#4cbf87] text-white cursor-pointer "
         />
         <Link to={"/"}>
-          <h1 className="text-md">Help Center</h1>
+          <h1 className="text-md">Reminders</h1>
+        </Link>
+      </div>
+      <div className="flex gap-3 items-center cursor-pointer hover:text-[#4cbf87]  mt-5">
+          <GoArchive
+          size={25}
+          className="hover:text-[#4cbf87] text-white cursor-pointer "
+        />
+        <Link to={"/"}>
+          <h1 className="text-md">Archive</h1>
+        </Link>
+      </div>
+      <div className="mt-5 flex gap-3 items-center cursor-pointer hover:text-[#4cbf87] ">
+        <IoTrashBinOutline 
+            size={25}
+            className="hover:text-[#4cbf87] text-white cursor-pointer"
+        />
+        <Link to={"/"}>
+          <h1 className="text-md">Bin</h1>
         </Link>
       </div>
     </div>
