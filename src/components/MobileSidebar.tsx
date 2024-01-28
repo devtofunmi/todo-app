b999b0e87a359fdc09c5174edcc687e950ace82a
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-01-29 00:47:32 +0100

    clean code

--- src/components/MobileSidebar.tsx
+++ src/components/MobileSidebar.tsx
@@ -8,10 +8,6 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 const MobileSidebar: FC = () => {
   return (
     <div className=" h-[300px] rounded-lg text-white p-3 bg-[#181819]  w-[250px] z-20 flex flex-col justify-left   border-[1px] border-white border-opacity-10">
-      <div>
-        {/* <img className="w-[100px]" src={dashboadlogo} alt="dashboard_logo" /> */}
-        {/* <h1>Notes</h1> */}
-      </div>
       <div className="mt-7 flex gap-3 items-center   rounded-lg cursor-pointer hover:text-[#4cbf87] ">
         <MdOutlineLightbulb
           size={25}
