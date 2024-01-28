49e6bc9345be573fd693ff29fcf36ff8a85444c8
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-01-28 23:24:58 +0100

    add if statement for the notes

--- src/components/NoteDetails.tsx
+++ src/components/NoteDetails.tsx
@@ -5,14 +5,18 @@ import '../App.css';
 
 
 interface NoteDetailsProps {
-  note: {
+  notes: {
     heading: string;
     description: string;
   };
   onClose: () => void;
 }
 
-const NoteDetails: React.FC<NoteDetailsProps> = ({ note, onClose }) => {
+const NoteDetails: React.FC<NoteDetailsProps> = ({ notes, onClose }) => {
+   if (!notes) {
+    return null; // You can return some placeholder or an empty div here
+  }
+
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="main text-white md:shadow-2xl w-full h-[140vh] md:h-screen mt-60 md:mt-0 overflow-scroll md:h-screen md:p-5 z-20 border-[0px] md:border-[1px] border-white border-opacity-10 bg-[#181819] p-8 z-60 ">
@@ -24,8 +28,8 @@ const NoteDetails: React.FC<NoteDetailsProps> = ({ note, onClose }) => {
             />
           </div>
 
-          <h2 className="text-[18px] font-bold">{note.heading}</h2>
-          <p className="text-[15px] mt-5">{note.description}</p>
+          <h2 className="text-[18px] font-bold">{notes.heading}</h2>
+          <p className="text-[15px] mt-5">{notes.description}</p>
         </div>
       </div>
     </div>
