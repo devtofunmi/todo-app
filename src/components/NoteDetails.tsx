3a0ba2f7b3e2b2a1270d075b926e6dc2c9cb1887
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-01-28 01:20:28 +0100

    add height for large screen

--- src/components/NoteDetails.tsx
+++ src/components/NoteDetails.tsx
@@ -15,7 +15,7 @@ interface NoteDetailsProps {
 const NoteDetails: React.FC<NoteDetailsProps> = ({ note, onClose }) => {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="main text-white md:shadow-2xl w-full h-[140vh] mt-60 md:mt-0 overflow-scroll md:h-screen md:p-5 z-20 border-[0px] md:border-[1px] border-white border-opacity-10 bg-[#181819] p-8 z-60 ">
+      <div className="main text-white md:shadow-2xl w-full h-[140vh] md:h-screen mt-60 md:mt-0 overflow-scroll md:h-screen md:p-5 z-20 border-[0px] md:border-[1px] border-white border-opacity-10 bg-[#181819] p-8 z-60 ">
         <div className="">
           <div className="flex items-center mb-4">
             <IoIosArrowBack
