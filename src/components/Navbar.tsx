2d0c7249e8a67d4faad67ad21d093aa1a82af3d9
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-02-02 14:16:12 +0100

    adjust the styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between'>
+    <div className='flex justify-between items-center'>
       <h1 className='text-white text-xl'>N Notes</h1>
        <div className="flex gap-[10px]  flex-col md:flex-row">
-            <button className=" text-white hover:text-[#4cbf87] px-10 py-2 rounded-md"
+            <button className=" text-white hover:text-[#4cbf87] px-10 py-2 rounded-md underline"
             >
               <Link to="/login">LogIn</Link>
             </button>
-            <button className="bg-black text-white hover:text-[#4cbf87] px-10 py-2 rounded-md"
+            <button className="border-[1px] border-white text-white hover:text-[#4cbf87] px-5 py-2 rounded-md"
             >
               <Link to="/signup">SignUp</Link>
             </button>
