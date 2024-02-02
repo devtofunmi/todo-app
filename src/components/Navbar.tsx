2256e4e3bb924bfcb791f50ecfbd99565463a0ab
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-02-02 14:32:21 +0100

    ..

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,11 +6,11 @@ const Navbar = () => {
     <div className='flex justify-between items-center'>
       <h1 className='text-white text-xl'>N Notes</h1>
        <div className="flex gap-[10px]  flex-col md:flex-row">
-            <button className=" text-white hover:text-[#4cbf87] px-10 py-2 rounded-md underline"
+            <button className=" text-white hover:text-[#4cbf87] px-10 py-1 rounded-md underline"
             >
               <Link to="/login">LogIn</Link>
             </button>
-            <button className="border-[1px] border-white text-white hover:text-[#4cbf87] px-5 py-2 rounded-md"
+            <button className="border-[1px] border-white text-white hover:text-[#4cbf87] px-5 py-1 rounded-md"
             >
               <Link to="/signup">SignUp</Link>
             </button>
