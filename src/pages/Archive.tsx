0934facbb2503d2646c11a2743877058b46e3548
devtofunmi <olayiwolajesutofunmi@gmail.com>
2024-02-06 19:04:27 +0100

    archive component

---
+++ src/pages/Archive.tsx
@@ -0,0 +1,15 @@
+import  { FC } from 'react'
+import DashboardLayout from '../layout/DashboardLayout'
+
+const Archive: FC = () => {
+  return (
+    <DashboardLayout>
+       <div className="main h-[90vh] mt-1 pb-3 md:bg-white md:shadow-2xl md:rounded-2xl md:bg-opacity-5 w-full md:mr-[-30px] md:w-[80%] md:p-5 border-[0px] md:border-[1px] border-white border-opacity-10 z-20 overflow-scroll">
+     
+        </div>
+
+    </DashboardLayout>
+  )
+}
+
+export default Archive
